345ca38fe34442df198d9d62c8db4a1795986848
theskillwithin <austinppeterson@gmail.com>
2018-04-21 11:32:44 -0600

    CRA

---
+++ src/api/currency.js
@@ -0,0 +1,6 @@
+import fetch from 'isomorphic-fetch';
+
+export const getDashCurrency = () => {
+  return fetch('https://insight.dash.org/api/currency').then(j => j.json())
+}
+
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { getDashCurrency } from '../api/currency';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currency: '',
+    }
+  }
+
+  componentDidMount() {
+    getDashCurrency()
+      .then(res => this.setState({ currency: res.data.dash_usd.toFixed(2) }))
+      .catch(err => this.setState({ currency: err }))
+  }
+  render() {
+    return (
+      <header>
+        <div className="container">
+          <div className="logo"><img src="/img/logo.png" alt=""/></div>
+          <h2>1 DASH = {this.state.currency} USD</h2>
+        </div>
+      </header>
+    )
+  }
+}
+
+export default Header
